b63126ef6784ca254b1e7f32bd425966c453243c
Nikhil Hira <nikhil.hira@ilabsolutions.com>
2015-10-10 16:34:33 -0700

    update endpoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
-git clone --depth 1 http://bitbucket.org/josemota/dockerapp app
+git clone --depth 1 https://github.com/dxypher/dockerapp.git
 
 cd app
 
+source "/usr/local/share/chruby/chruby.sh"
+chruby ruby
+
+gem install bundler
+
 bundle install
+bundle exec rake db:migrate
 
 if [[ $/ != 0 ]]; then
   echo
